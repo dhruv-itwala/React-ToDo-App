e11ae8e00a73338c2afdf060f28110609d3d575b
dhruv06122002 <dhruvitwala@gmail.com>
2024-09-04 12:53:46 +0530

    Container

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,8 +1,16 @@
-
-
 function App() {
+  return (
+    <center class="todo-container">
+      <h1> Todo App</h1>
 
-  return <div>PlaceHolder</div>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-8">col-8</div>
+          <div class="col-4">col-4</div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
